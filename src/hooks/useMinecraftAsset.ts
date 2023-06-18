118ef8180c6b7ffa02cca8dfc94c1d9187e3b336
Seol Park <9481405+chalkpe@users.noreply.github.com>
2023-06-18 15:25:12 +0900

    feat: force upgrade 1.19 to 1.20.1

--- src/hooks/useMinecraftAsset.ts
+++ src/hooks/useMinecraftAsset.ts
@@ -1,7 +1,7 @@
 import { useCallback } from 'react'
 
 const useMinecraftAsset = (version: string) =>
-    useCallback((item: string) => `https://mc.nerothe.com/img/${version === '1.19' ? '1.19.2' : version}/${item}.png`, [version])
+    useCallback((item: string) => `https://mc.nerothe.com/img/${version === '1.19' ? '1.20.1' : version}/${item}.png`, [version])
     // useCallback((path: string) => `https://assets.mcasset.cloud/${version}/assets/${path}`, [version])
 
 export default useMinecraftAsset
